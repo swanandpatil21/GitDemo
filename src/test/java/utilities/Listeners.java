5ee0f468f662ca84d8f7b9f6173830c3a2062757
swanandpatil21 <swanandpatil21@gmail.com>
2022-08-12 22:18:23 +0530

    first commit

---
+++ src/test/java/utilities/Listeners.java
@@ -0,0 +1,28 @@
+package utilities;
+
+
+import java.io.IOException;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import tests.BaseTest;
+
+public class Listeners extends BaseTest implements ITestListener{
+
+	public void onTestFailure(ITestResult result) {
+		
+		try {
+			Screenshot.captureScreen(driver, result.getName());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	
+
+}
